8b292e13860cbd6e592bd475147a6bb6e9213511
Bedra Kraja <bedra.kraja22@gmail.com>
2022-09-23 08:51:47 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-
+  const [user, setUser] = useState(null)
   return (
     <div className="App">
       <h1>👋 WELCOME 👋</h1>
       <h2>Please sign in</h2>
       <form
         className='sign-up-form'
+        onSubmit={event => {
+          event.preventDefault()
+          fetch('http://localhost:5000/sign-up', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              email: event.target.email.value,
+              password: event.target.email.value
+            })
+          })
+            .then(resp => resp.json())
+        }}
       >
         <h2>Sign Up Form</h2>
         <label>
@@ -27,6 +38,21 @@ function App() {
 
       <form
         className='log-in-form'
+        onSubmit={event => {
+          event.preventDefault()
+
+          fetch('http://localhost:5000/log-in', {
+            method: 'POST',
+            headers: {
+              'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+              email: event.target.email.value,
+              password: event.target.password.value
+            })
+          })
+            .then(resp => resp.json())
+        }}
       >
         <h2>Log In Form</h2>
 
